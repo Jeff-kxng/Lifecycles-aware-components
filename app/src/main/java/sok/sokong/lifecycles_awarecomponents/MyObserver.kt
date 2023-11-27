2b02c467a094eca56e0091cf6a7a4e6d7e0412f1
Jeff-kxng <sokong.s211005@sis.hust.edu.vn>
2023-11-27 18:03:55 +0700

    Initial commit

---
+++ app/src/main/java/sok/sokong/lifecycles_awarecomponents/MyObserver.kt
@@ -0,0 +1,34 @@
+package sok.sokong.lifecycles_awarecomponents
+
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+class MyObserver : LifecycleObserver {
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onCreateEvent() {
+        Log.d("MyObserver", "onCreateEvent") // Thay đổi thông điệp log ở đây
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun OnResumeEvent() {
+        Log.d("MyObserver", "onResume")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun onPauseEvent() {
+        Log.d("MyObserver", "onPauseEvent")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onStopEvent() {
+        Log.d("MyObserver", "onStopEvent")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onDestroyEvent() {
+        Log.d("MyObserver", "onDestroyEvent")
+    }
+}
